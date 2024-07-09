2a435089e7030029f5da8ce51962085b66a73cc3
AyushKumarSrivas <117811777+AyushKumarSrivas@users.noreply.github.com>
2024-07-09 20:18:37 +0530

    Add files via upload

---
+++ assignment1.c
@@ -0,0 +1,103 @@
+/*1.print Hello Students on the screen
+
+int main()
+{
+printf("Hello Students");
+}
+*/
+
+//2.print hello in the first line and students in the second line
+
+
+/*int main()
+{
+    printf("Hello\nStudents");
+}
+*/
+
+//3.Print "MySirG" on the screen.(Remember to print in the double quotes)
+/*int main()
+{
+    printf("\"MySirG\"");
+}
+*/
+
+//4.Find the area of the circle
+/*int main()
+{
+    float r,area;
+    printf("Enter the radius of the circle:");
+    scanf("%f",&r);
+    area=3.14*r*r;
+    printf("Area of the circle is %.2f",area);
+}
+*/
+
+//5.Calculate the length of string using printf function
+
+/*int main()
+{
+    int x=printf("Ayush");
+    printf("\nLength of the string is %d",x);
+}
+*/
+
+//6.To print the name of the user in double quotes
+
+/*int main()
+{
+    printf("\"Hello,Ayush\"");
+}
+*/
+
+//7.To print "%d" on the screen
+/*int main()
+{
+    printf("\"%%d\"");
+}
+*/
+
+//8.To print "\n" on the screen
+/*int main()
+{
+    printf("\"\\n\"");
+}
+*/
+
+//9.To print "\\" on the screen
+/*int main()
+{
+    printf("\"\\\\\"");
+}
+*/
+
+//10.To take date as an input
+/*int main()
+{
+    int DD,MM,Year;
+    printf("Enter date as DD/MM/Year:");
+    scanf("%d/%d/%d",&DD,&MM,&Year);
+    printf("Day=%d,Month=%d,Year=%d",DD,MM,Year);
+}
+*/
+
+//11.To take time as an input
+/*int main()
+{
+    int HH,MM;
+    printf("Enter time as HH:MM:");
+    scanf("%d:%d",&HH,&MM);
+    printf("%d Hour and %d Minute",HH,MM);
+}
+*/
+
+//12.Find output of below code
+/*int main()
+{
+    int x=printf("ineuron");
+    printf("%d",x);
+    return 0;
+}
+
+Output:- ineuron7
+*/
